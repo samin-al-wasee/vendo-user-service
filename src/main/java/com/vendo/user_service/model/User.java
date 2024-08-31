4932cf15d725fd100d2b384c4b81a9a00197f1ff
Mohammad Samin-Al-Wasee <sameenwasee@gmail.com>
2024-08-31 09:25:37 +0600

    Add User model with migrations

---
+++ src/main/java/com/vendo/user_service/model/User.java
@@ -0,0 +1,32 @@
+package com.vendo.user_service.model;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.UuidGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+public class User {
+    @Id
+    @UuidGenerator
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+}
---
+++ src/main/resources/db/migration/V2__Create_users_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
