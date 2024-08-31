02dd132bc2eb810738f08d6d5cbf35f0d4d3db61
Mohammad Samin-Al-Wasee <sameenwasee@gmail.com>
2024-08-31 11:42:13 +0600

    Add unit tests for User model

---
+++ src/main/java/com/vendo/user_service/constant/ValidationConstants.java
@@ -0,0 +1,6 @@
+package com.vendo.user_service.constant;
+
+public class ValidationConstants {
+    public static final String NAME_PATTERN = "^[a-zA-Z.-]+$";
+    public static final String EMAIL_PATTERN = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+}
--- src/test/java/com/vendo/user_service/UserServiceApplicationTests.java
+++ src/test/java/com/vendo/user_service/UserServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.vendo.user_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserServiceApplicationTests {
 
 	@Test
