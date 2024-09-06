edbcbf71b0c2a6d7b43874653d9269328586fdb2
Mohammad Samin-Al-Wasee <sameenwasee@gmail.com>
2024-09-06 19:00:49 +0600

    Fix User model test and setup a dockerized test db

--- src/test/java/com/vendo/user_service/UserServiceApplicationTests.java
+++ src/test/java/com/vendo/user_service/UserServiceApplicationTests.java
@@ -1,10 +1,12 @@
 package com.vendo.user_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 class UserServiceApplicationTests {
 
